acccd0f3d524177bfbbc66b9986973241803ee00
Fran Hall <fhall92@gmail.com>
2019-11-26 10:35:42 +0000

    updated register_action

--- register_action.php
+++ register_action.php
@@ -14,17 +14,29 @@ if (isset($_POST['register-submit'])){
         header("Location: ../register.php?error=emptyfields&username=".$username);
         exit();
     }
+
+    //Only allow a-z, A-Z, 0-9 in username
+    else if(!preg_match("/^[a-zA-Z0-9]*$/")){
+        header("Location: ../register.php?error=invalidusername&username=".$username);
+    }
    
-    //If no errors caught, register the user
+    //If no errors caught, check if user already exists in db
     else{
 
         $sql = "SELECT users FROM registration WHERE username=?";
         $stmt = mysqli_stmt_init($conn);
 
         if(!mysqli_stmt_prepare($stmt, $sql)){
-            header("Location: ../")
+            header("Location: ../register.php?error=sqlerror");
+            exit();
         }
-        
+
+        else{
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            $resultCheck = mysqli)stmt_num_rows($stmt);
+        }
     }
 
 }
